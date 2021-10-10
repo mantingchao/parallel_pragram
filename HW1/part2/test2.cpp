645ba39eb05bdf014ba99937545c5e5ad1a738f5
Manting <51501170+mantingchao@users.noreply.github.com>
2021-10-10 22:29:48 +0800

    update part2

--- HW1/part2/test2.cpp
+++ HW1/part2/test2.cpp
@@ -5,24 +5,37 @@
 void test2(float *__restrict a, float *__restrict b, float *__restrict c, int N)
 {
   __builtin_assume(N == 1024);
-  a = (float *)__builtin_assume_aligned(a, 16);
-  b = (float *)__builtin_assume_aligned(b, 16);
+  a = (float *)__builtin_assume_aligned(a, 32);
+  b = (float *)__builtin_assume_aligned(b, 32);
+  c = (float *)__builtin_assume_aligned(c, 32);
 
-  fasttime_t time1 = gettime();
-  for (int i = 0; i < I; i++)
+  int count = 50;
+  double elapsedf = 0;
+  for (int t = 0; t < count; t++)
   {
-    for (int j = 0; j < N; j++)
+    fasttime_t time1 = gettime();
+    for (int i = 0; i < I; i++)
     {
-      /* max() */
-      c[j] = a[j];
-      if (b[j] > a[j])
-        c[j] = b[j];
+      for (int j = 0; j < N; j++)
+      {
+        /* max() */
+        // c[j] = a[j];
+        // if (b[j] > a[j])
+        //   c[j] = b[j];
+
+        if (b[j] > a[j])
+          c[j] = b[j];
+        else
+          c[j] = a[j];
+      }
     }
+
+    fasttime_t time2 = gettime();
+
+    elapsedf += tdiff(time1, time2);
   }
 
-  fasttime_t time2 = gettime();
-
-  double elapsedf = tdiff(time1, time2);
+  elapsedf /= count;
   std::cout << "Elapsed execution time of the loop in test2():\n"
             << elapsedf << "sec (N: " << N << ", I: " << I << ")\n";
 }
--- HW1/part2/test3.cpp
+++ HW1/part2/test3.cpp
@@ -2,23 +2,33 @@
 #include "test.h"
 #include "fasttime.h"
 
-double test3(double* __restrict a, int N) {
+double test3(double *__restrict a, int N)
+{
   __builtin_assume(N == 1024);
   a = (double *)__builtin_assume_aligned(a, 16);
 
+  int count = 50;
+  double elapsedf = 0;
   double b = 0;
+  for (int t = 0; t < count; t++)
+  {
+    b = 0;
 
-  fasttime_t time1 = gettime();
-  for (int i=0; i<I; i++) {
-    for (int j=0; j<N; j++) {
-      b += a[j];
+    fasttime_t time1 = gettime();
+    for (int i = 0; i < I; i++)
+    {
+      for (int j = 0; j < N; j++)
+      {
+        b += a[j];
+      }
     }
-  }
-  fasttime_t time2 = gettime();
+    fasttime_t time2 = gettime();
 
-  double elapsedf = tdiff(time1, time2);
-  std::cout << "Elapsed execution time of the loop in test3():\n" 
-    << elapsedf << "sec (N: " << N << ", I: " << I << ")\n";
+    elapsedf += tdiff(time1, time2);
+  }
+  elapsedf /= count;
+  std::cout << "Elapsed execution time of the loop in test3():\n"
+            << elapsedf << "sec (N: " << N << ", I: " << I << ")\n";
 
   return b;
 }
