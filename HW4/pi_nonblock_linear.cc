a9b01e20384368cd86d6a15959dc06a6997d6031
Manting <51501170+mantingchao@users.noreply.github.com>
2021-12-01 20:54:51 +0800

    non-blocking linear

--- HW4/pi_nonblock_linear.cc
+++ HW4/pi_nonblock_linear.cc
@@ -16,23 +16,53 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int source;
+    int dest;
+    int tag = 0;
+
+    long long int number_of_tosses = tosses / world_size;
+    long long int number_in_circle = 0;
+    long long int total = 0;
+    float x = 0.0f, y = 0.0f;
+    unsigned int seed = time(NULL) + world_rank;
+
+    MPI_Request reqs[world_size - 1];
+    MPI_Status status[world_size - 1];
+
+    for (long long int toss = 0; toss < number_of_tosses; toss++)
+    {
+        x = rand_r(&seed) / ((float)RAND_MAX);
+        y = rand_r(&seed) / ((float)RAND_MAX);
+        if (x * x + y * y <= 1.0f)
+            number_in_circle++;
+    }
 
     if (world_rank > 0)
     {
         // TODO: MPI workers
+        dest = 0;
+        MPI_Isend(&number_in_circle, 1, MPI_LONG_LONG_INT, dest, world_rank, MPI_COMM_WORLD, &reqs[world_rank]);
     }
     else if (world_rank == 0)
     {
         // TODO: non-blocking MPI communication.
         // Use MPI_Irecv, MPI_Wait or MPI_Waitall.
-        MPI_Request requests[];
+        total = number_in_circle;
+        for (source = 1; source < world_size; source++)
+        {
+            MPI_Irecv(&number_in_circle, 1, MPI_LONG_LONG_INT, source, source, MPI_COMM_WORLD, &reqs[source - 1]);
+            total += number_in_circle;
+        }
 
-        MPI_Waitall();
+        MPI_Waitall(world_size - 1, reqs, status);
     }
 
     if (world_rank == 0)
     {
         // TODO: PI result
+        pi_result = 4 * total / ((float)tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
