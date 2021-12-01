fb288f8c6bf5812b40eb50279585358ff0fe64ac
Manting <51501170+mantingchao@users.noreply.github.com>
2021-12-01 22:56:44 +0800

    add mpi reduce

--- HW4/pi_reduce.cc
+++ HW4/pi_reduce.cc
@@ -16,12 +16,33 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int tag = 0;
+
+    long long int number_of_tosses = tosses / world_size;
+    long long int number_in_circle = 0;
+    long long int total = 0;
+    float x = 0.0f, y = 0.0f;
+    unsigned int seed = time(NULL) + world_rank;
+
+    long long int *recv = NULL;
+
+    for (long long int toss = 0; toss < number_of_tosses; toss++)
+    {
+        x = rand_r(&seed) / ((float)RAND_MAX);
+        y = rand_r(&seed) / ((float)RAND_MAX);
+        if (x * x + y * y <= 1.0f)
+            number_in_circle++;
+    }
 
     // TODO: use MPI_Reduce
+    MPI_Reduce(&number_in_circle, &total, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (world_rank == 0)
     {
         // TODO: PI result
+        pi_result = 4 * total / ((float)tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
