b8ee33558c192eae3a88e1c920d54cfcc756348e
Manting <51501170+mantingchao@users.noreply.github.com>
2021-12-29 20:42:18 +0800

    update

--- HW6_openCL/hostFE.c
+++ HW6_openCL/hostFE.c
@@ -7,21 +7,27 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             float *inputImage, float *outputImage, cl_device_id *device,
             cl_context *context, cl_program *program)
 {
+    // initialize device
     cl_int status;
     int filterSize = filterWidth * filterWidth * sizeof(float);
     int imageSize = imageHeight * imageWidth * sizeof(float);
 
+    // context create a command queue
     cl_command_queue commandQueue = clCreateCommandQueue(*context, *device, 0, NULL);
 
+    // create buffers on device
     cl_mem inputBuffer = clCreateBuffer(*context, CL_MEM_READ_ONLY, imageSize, NULL, NULL);
     cl_mem fliterBuffer = clCreateBuffer(*context, CL_MEM_READ_ONLY, filterSize, NULL, NULL);
     cl_mem outputBuffer = clCreateBuffer(*context, CL_MEM_WRITE_ONLY, imageSize, NULL, NULL);
 
+    // transfer input data to the device
     clEnqueueWriteBuffer(commandQueue, inputBuffer, CL_TRUE, 0, imageSize, inputImage, 0, NULL, NULL);
     clEnqueueWriteBuffer(commandQueue, fliterBuffer, CL_TRUE, 0, filterSize, filter, 0, NULL, NULL);
 
+    // select kernel
     cl_kernel kernel = clCreateKernel(*program, "convolution", NULL);
 
+    // set arguments
     clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputBuffer);
     clSetKernelArg(kernel, 1, sizeof(cl_mem), &outputBuffer);
     clSetKernelArg(kernel, 2, sizeof(cl_mem), &fliterBuffer);
@@ -31,6 +37,8 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
 
     size_t globalItemSize = imageHeight * imageWidth;
     size_t localItemSize = 64;
+    // execute kernel
     clEnqueueNDRangeKernel(commandQueue, kernel, 1, NULL, &globalItemSize, &localItemSize, 0, NULL, NULL);
+    // copy results from device back to host
     clEnqueueReadBuffer(commandQueue, outputBuffer, CL_TRUE, 0, imageSize, outputImage, 0, NULL, NULL);
 }
